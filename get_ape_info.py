12214768ab32f405afb45bc714fe87c04545eff1
Noah Burget <nburget12@gmail.com>
2026-03-16 15:54:49 -0400

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -19,6 +19,7 @@ api_url = "https://mainnet.infura.io/v3/ca7bf0cd2f3648c89f9bfd95993f9fcf"  # YOU
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
+contract = web3.eth.contract(address=contract_address, abi=abi)
 
 def get_ape_info(ape_id):
     assert isinstance(ape_id, int), f"{ape_id} is not an int"
